917ca5f47ae9ca7f19d406b357488be98a59067e
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-21 18:38:46 +0530

    comment remove

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,13 +9,5 @@ export default {
         locale: 'en_IN',
         url: 'mridul.tech',
         site_name: 'SuperHero App',
-        // images: [
-        //     {
-        //         url: "",
-        //         width: 300,
-        //         height: 300,
-        //         alt: "Next Template",
-        //     }
-        // ],
     }
 };
